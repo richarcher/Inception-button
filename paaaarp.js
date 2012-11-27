eb34c216d7d6fda66231b06561722659091ad64e
Richard Archer <richard.archer@unboxedconsulting.com>
2012-11-27 09:19:06 +0200

    Make it, y'know, *work*

--- paaaarp.js
+++ paaaarp.js
@@ -1,9 +1,14 @@
-(function(){
+// (function(){
 
-  var background = chrome.extension.getURL("inception.mp3");
-  var audio = new Audio(background);
+//   var background = chrome.extension.getURL("inception.mp3");
+//   var audio = new Audio(background);
 
-    audio.play();
-    return audio;
+//     audio.play();
+//     return audio;
 
-})();
+//   // console.log('whoop');
+// })();
+
+// function yo() {
+//   console.log("Yo");
+// }
